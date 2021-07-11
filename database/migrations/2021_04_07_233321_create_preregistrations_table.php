d8c6f0e8dc84d17a1465a6b4e9b869f8903c60eb
Milton <mefa.milton@gmail.com>
2021-07-11 18:37:11 -0500

    Arreglada tabla preregistrations

--- database/migrations/2021_04_07_233321_create_preregistrations_table.php
+++ database/migrations/2021_04_07_233321_create_preregistrations_table.php
@@ -31,6 +31,6 @@ class CreatePreregistrationsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('preregistration');
+        Schema::dropIfExists('preregistrations');
     }
 }
